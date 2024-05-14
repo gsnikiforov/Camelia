807d6a025107708cc3e1d1a372c212625473f7ad
gsnikiforov <goshaniker38@gmail.com>
2024-05-14 17:31:01 +0300

    обновил тесты

--- project/test_ciphers.py
+++ project/test_ciphers.py
@@ -1,53 +1,25 @@
 """
-Юнит-тест для режимов шифрования.
+Юнит-тест для алгоритма Camellia.
 """
 
 import unittest
 
-from utilities import DECRYPT
-
-from ofb import ofb
-from mgm import mgm_encrypt, mgm_decrypt
-
-from camellia import camellia
+from utilities import bitsize, DECRYPT
+from ciphers.camellia import camellia
 
 from random import randint
 
 
-class TestCipherModes(unittest.TestCase):
-    def test_ofb_camellia(self):
+class TestCiphers(unittest.TestCase):
+    def test_camellia(self):
         """
-        Проверка шифрования в режиме OFB
-        при помощи алгоритма Camellia
-        со случайно генерируемым ключом.
+        Проверка алгоритма Camellia.
         """
+        
+        key = randint(10000, 100000000)
+        message = 0x0123456789abcdeffedcba9876543210
 
-        iv = 0x80000000000000000000000000000000
-        me = 0x10000000000000000000000000000000
-        ke = randint(100000, 1000000000)
+        c, key, r_t = camellia(message, key)
+        m, key, r_t = camellia(c, key, DECRYPT, r_t)
 
-        cipher, res_type = ofb(camellia, me, ke, iv)
-        decipher, _ = ofb(camellia, cipher, ke, iv, DECRYPT, res_type)
-
-        self.assertEqual(hex(decipher), hex(me))
-
-
-    def test_mgm_camellia(self):
-        """
-        Проверка шифрования в режиме MGM
-        при помощи алгоритма Camellia
-        со случайно генерируемым ключом.
-        """
-
-        n = 128
-        nonce = 0x6 << (n-4)
-        message = "Hello darkness, my old friend"
-        ass_data = 0xbfee5c7c5fe97718ed6bf376739259fa
-
-        key = randint(100000, 1000000000)
-        c, a_d, t, r, r_t = mgm_encrypt(camellia, nonce,
-                                  message, ass_data, key)
-        m, a = mgm_decrypt(camellia, nonce, c, ass_data,
-                           key, t, r, r_t)
-
-        self.assertEqual(message, m)
+        self.assertEqual(m, message)
